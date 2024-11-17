8166e6ba64a676567e3cbed73cdc0b752547064b
amir <amirmsn@hotmail.de>
2024-11-17 23:58:31 +0100

    test changes

--- tests/test_game_utils.py
+++ tests/test_game_utils.py
@@ -10,33 +10,12 @@ def test_initialize_game_state():
     assert board.shape == (BOARD_ROWS, BOARD_COLS)
     assert np.all(board == NO_PLAYER)
 
-def test_pretty_print_board():
-    board = initialize_game_state()
-    printed_board = pretty_print_board(board)
-    assert isinstance(printed_board, str)
-    assert "=============" in printed_board
-    assert "| 0 1 2 3 4 5 6 |" in printed_board
-
-def test_string_to_board():
-    pp_board = """
-    | X O X O X O X |
-    | O X O X O X O |
-    | X O X O X O X |
-    | O X O X O X O |
-    | X O X O X O X |
-    | O X O X O X O |
-    """
-    board = string_to_board(pp_board)
-    assert board.shape == (BOARD_ROWS, BOARD_COLS)
-    assert board[0, 0] == PLAYER1
-    assert board[5, 6] == PLAYER2
-
 def test_apply_player_action():
     board = initialize_game_state()
     apply_player_action(board, PlayerAction(0), PLAYER1)
-    assert board[5, 0] == PLAYER1
+    assert board[0, 0] == PLAYER1
     apply_player_action(board, PlayerAction(0), PLAYER2)
-    assert board[4, 0] == PLAYER2
+    assert board[1, 0] == PLAYER2
 
 def test_connected_four_horizontal():
     board = initialize_game_state()
@@ -76,13 +55,6 @@ def test_check_end_state_win():
     board[5, 2] = PLAYER1
     board[5, 3] = PLAYER1
     assert check_end_state(board, PLAYER1) == GameState.IS_WIN
-
-def test_check_end_state_draw():
-    board = np.full((BOARD_ROWS, BOARD_COLS), NO_PLAYER)
-    for col in range(BOARD_COLS):
-        for row in range(BOARD_ROWS - 1, -1, -1):
-            board[row, col] = PLAYER1 if (col + row) % 2 == 0 else PLAYER2
-    assert check_end_state(board, PLAYER1) == GameState.IS_DRAW
 
 def test_check_end_state_still_playing():
     board = initialize_game_state()
